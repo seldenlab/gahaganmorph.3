b9dd2030c66022490e21ffecbb914b943fa7d389
Robert Z. Selden, Jr <zac_selden@pm.me>
2021-12-02 09:41:36 -0600

    <edit ser>

--- supp/tinker.R
+++ supp/tinker.R
@@ -1,29 +1,64 @@
+# Seriation
+
+data <- read.csv("gahagan-diagnostics.csv")
+data$context <- as.character(data$context)
+rowSums(data[, 3:15])
+colSums(data[, 3:15])
+
+library(ca)
+data.ca <- ca(data[, 3:15])
+plot.ca(data.ca, cex=.75)
+
+library(vegan)
+data.dec <- decorana(data[, 3:15])
+plot(data.dec, display="both")
+
+library(plotrix)
+rowlbl <- data$context
+idx <- rev(order(data.ca$rowcoord[, 1]))
+data.pct <- prop.table(as.matrix(data[, 3:15]), 1) * 100
+battleship.plot(data.pct[idx, ], 
+                cex.labels = .75, 
+                maxxspan=.75,
+                yaxlab=rowlbl[idx], 
+                col="gray")
+
+idx2 <- rev(order(data.dec$rproj[, 1]))
+battleship.plot(data.pct[idx2, ], 
+                cex.labels = .75, 
+                maxxspan=.75,
+                yaxlab=rowlbl[idx2], 
+                col="gray")
+
+plot(idx, idx2, pch=16)
+abline(a=0, b=1)
+
 # Mortuary Assemblage Diversity
 
 data <- read.csv("gahagan-diagnostics.csv")
 
 library(vegan)
 # assemblage size (N)
-N <- rowSums(data[3:14])
+N <- rowSums(data[3:15])
 N ## assemblage sizes range from 8 - 184
 
 # how many of each type were found?
-T <- colSums(data[3:14])
+T <- colSums(data[3:15])
 T ## quantity of each type (T) found across contexts
 
 # data by percentage
-data.pct <- data[3:14]/N*100
+data.pct <- data[3:15]/N*100
 # mean percent (Mp) of each type across assemblage
 Mp <- colMeans(data.pct)
 Mp
 
 #diversity, richness
 # richness (S) = number of types in assemblage
-S <- specnumber(data[3:14])
+S <- specnumber(data[3:15])
 S
 
 # ubiquity (U) = number of assemblages that contain a particular type
-U <- specnumber(data[3:14])
+U <- specnumber(data[3:15])
 U
 
 ## percentage of sites that have each type
@@ -31,18 +66,18 @@ Up <- U/length(N)*100
 Up
 
 # Shannon diversity
-H <- diversity(data[3:14])
+H <- diversity(data[3:15])
 ## high diversity = more types and spread more evenly over types
 H
 
 # Simpson index
-D1 <- diversity(data[3:14], 
+D1 <- diversity(data[3:15], 
                 index = "simpson")
 ## probability that two artifacts drawn randomly represent different types
 D1
 
 # inverse Simpson index
-D2 <- diversity(data[3:14], 
+D2 <- diversity(data[3:15], 
                 index = "invsimpson")
 ## effective number of types
 D2
@@ -107,6 +142,17 @@ lines(xval,
                   data.frame(N = xval))), 
       lty = 2)
 
+# nonlinear least squares
+data.nls <- nls(S ~ SSasymp(N, Asym, R0, lrc))
+summary(data.nls)
+deviance(data.nls)
+lines(xval, predict(data.nls, data.frame(N = xval)), lty = 3, lwd = 2)
+legend("bottomright",
+       c("Logarithmic", "Power (log-log)", "Asymptotic Curve"),
+       lty = 1:3,
+       lwd = c(1, 1, 2),
+       bg = "white")
+
 # rarefaction curve
 ## how do individual assemblages compare to the composite?
 xval <- seq(2, 200, by = 2)
@@ -129,7 +175,7 @@ identify(N, S, rownames(data))
 
 ## how different are the smaller samples from the largest one?
 data.rare <- data[1,3:14]
-data.184 <- rarefy(data.rare[1,], xval[1:8], se = TRUE)
+data.184 <- rarefy(data.rare[1,], xval[1:7], se = TRUE)
 Est <- data.184[1, ]
 Sd <- data.184[2, ]
 rare184 <- cbind(lower = Est-2*Sd,
@@ -138,6 +184,6 @@ rare184 <- cbind(lower = Est-2*Sd,
 
 plot(S~N,
      ylim = range(rare184),
-     xlim = range(xval[1:8]),
+     xlim = range(xval[1:7]),
      pch = 16)
-
+identify(N, S, rownames(data))
