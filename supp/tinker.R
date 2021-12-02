242d078da9088f9644e5273e66065912a513cb5e
Robert Z. Selden, Jr <zac_selden@pm.me>
2021-12-02 11:16:19 -0600

    <shift>

--- supp/tinker.R
+++ supp/tinker.R
@@ -187,3 +187,41 @@ plot(S~N,
      xlim = range(xval[1:7]),
      pch = 16)
 identify(N, S, rownames(data))
+
+
+
+
+```{r h1a, out.width = "100%", dpi = 300, echo=TRUE, warning=FALSE, fig.cap="Stratigraphic position of Burial Pits 1 and 2 at the Mounds Plantation site; adapted from Webb (1975:Figure 7). Burial Pit 2 is the only burial found to be intrusive from the mound surface."}
+knitr::include_graphics('images/h1a.jpg')
+```
+
+```{r h1a.box, out.width = "100%", dpi = 300, echo=TRUE, warning=FALSE, fig.cap="Difference in centroid size between Gahagan bifaces from earlier (BPs 1, 5, and 8) and later (BP 2) contexts at the Mounds Plantation site."}
+knitr::include_graphics('images/mp-size.png')
+```
+
+#### _Hypothesis 1b: Temporal change in Caddo preference at George C. Davis_
+
+To assess the temporal change in preference between caches of Gahagan bifaces recovered from Mound C at the George C. Davis site, those from Feature 134 are contrasted with those from Feature 119 [@RN3682;@RN8186]. The stratigraphic position of Feature 119 indicates that the burials in that feature occurred subsequent to those associated with Feature 134 [@RN3682;@RN8186].
+
+```{r h1b, out.width = "100%", dpi = 300, echo=TRUE, warning=FALSE, fig.cap="Stratigraphic position of Features 119 and 134 at the George C. Davis site; adapted from Story (1997:Figure 13)."}
+knitr::include_graphics('images/h1b.jpg')
+```
+
+##### _Hypothesis 1b Findings_:
+
+- Gahagan bifaces from F134 and F119 do not differ in _shape_
+- Gahagan bifaces from F134 and F119 do not differ in _size_
+
+```{r h1b.box, out.width = "100%", dpi = 300, echo=TRUE, warning=FALSE, fig.cap="Difference in centroid size between Gahagan bifaces from earlier (F134) and later (F119) contexts at the George C. Davis site."}
+knitr::include_graphics('images/gcd-size.png')
+```
+
+#### Hypothesis 1: Summary of results
+
+Hypothesis 1 was tested using two samples of Gahagan bifaces from Caddo burial contexts at the Mounds Plantation and George C. Davis sites where stratigraphy dictates differing (earlier/later) temporal positions. There is an important cultural distinction between archaeological contexts at Mounds Plantation and George C. Davis, in that all of the Gahagan bifaces from Mounds Plantation were recovered _in association with an individual_ (Burials 1, 2, 5, and 8) [@RN8174], and all but two of the Gahagan bifaces from George C. Davis articulate with _caches included along the northern periphery of two group burials_ (Features 119 and 134) [@RN3682;@RN8186].
+
+Previous studies have demonstrated a significant difference in shape between Gahagan bifaces recovered from the Mounds Plantation and George C. Davis sites [@RN11783]. In burials at Mounds Plantation, the Caddo were `selecting` for Gahagan bifaces that were significantly smaller in later contexts; a pattern that is not present at George C. Davis.
+
+The results raise substantive questions regarding the cultural or social practices that were driving the temporal shift in size at Mounds Plantation. Might this size difference articulate with a shift in trading or exchange-based relationships with central Texas groups, and/or might the shift be related to a shift in functional use?
+  
+  At both sites, shape remains consistent and does not differ between contexts, indicating an established `shape preference` that may have shifted at Mounds Plantation due to cyclical differences in the variable social mechanisms associated with raw material procurement. It may also be the case that the size difference was related to signaling, as well as the intended audience. For instance, larger bifaces selected for inclusion with caches may suggest that the intended audience was the group; representative of a potentially ostentatious display. The smaller Gahagan bifaces from individual burials may have been personal items belonging to the deceased, intended to signal status among personal contacts.
